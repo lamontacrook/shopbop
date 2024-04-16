1aa03b3d181c73b218461339430baa7a51e7414b
KP Parrish <krparrish@adobe.com>
2024-04-16 12:02:59 -0600

    nav updates

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import logoPath from '../../media/shopbop-logo.png';
+import amazonPrime from '../../media/amazon-prime.svg';
+import filledHeart from '../../media/filled-heart.svg';
+import bag from '../../media/bag.svg';
 import './header.css';
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div>
+    <div className='header-wrapper'>
+      <div className='info-bar-wrapper'>
+        <div className='info-bar'>
+          <div className='metadata'>
+            <button className='location'>US</button>
+            <button className='language'>EN</button>
+            <button className-='currency'>$USD</button>
 
+          </div>
+          <div className='prime-banner'>
+            <span>FREE shipping and FREE returns</span>
+            <img src={amazonPrime}/>
+          </div>
+
+          <div className='account'>
+            <a className='login'>Sign In/Register</a>
+            <button className='favorites'></button>
+            <button className='bag'></button>
+          </div>
+        </div>
       </div>
-      <div>
+      <div className='logo-wrapper'>
         <a href="">
           <img src={logoPath} />
         </a>
